aa30c3c5b570b1925b557d68e0c9724d0f11719f
tanner-sundeen <45770821+tanner-sundeen@users.noreply.github.com>
2018-12-10 15:02:50 -0600

    Add files via upload

--- CaesarCipher/src/caesarCipher/CaesarCipher.java
+++ CaesarCipher/src/caesarCipher/CaesarCipher.java
@@ -6,35 +6,41 @@ public class CaesarCipher {
 	
 	public static void main(String args[]) {
 		Scanner input = new Scanner(System.in);
-		int shiftk = 11; //shift key
+		int shiftk = 11; //preset shift key
 		int i;
 		String pstr; //plaintext entered by user
-		String estr = "";
-		String dstr = "";
+		String estr = ""; //text after encryption
+		String dstr = ""; //encrypted text decrypted again
 		
+		//user enters plaintext
 		System.out.println("Enter plaintext here:");
 		pstr = input.nextLine();
-		pstr = pstr.toLowerCase();
+		pstr = pstr.toLowerCase(); //all capital letters become lowercase
 		
-		char ch1[] = pstr.toCharArray();
-		char ch3,ch4;
+		char ch1[] = pstr.toCharArray(); //represents each character in pstr
+		char ch3,ch4; //represents each character in text after encryption and decryption, respectively
 		System.out.println("Plaintext encrypted:");
 		
+		//encrypter runs until there are no characters remaining
 		for(i=0;i<pstr.length();i++) {
+			//changes character by shift key if it is a letter
 			if(Character.isLetter(ch1[i])) {
-				ch3 = (char)(((int)ch1[i]+shiftk-97)%26+97);
+				ch3 = (char)(((int)ch1[i]+shiftk-97)%26+97); //En(x) = (x+n) mod 26, alphabet begins at 97
 				estr=estr+ch3;
 			}
+			//leaves a space if the character is already a space
 			else if(ch1[i] == ' ') {
 				estr=estr+ch1[i];
 			}
 		}
 		System.out.println(estr);
 		
-		char ch2[] = estr.toCharArray();
+		char ch2[] = estr.toCharArray(); //represents each character in estr
 		
 		System.out.println("Encrypted text decrypted:");
+		//decryption is similar to encryption, but subtracts by the shift key
 		for(i=0;i<estr.length();i++) {
+			//two different equations to prevent character from leaving alphabet
 			if(Character.isLetter(ch2[i])) {
 				if(((int)ch2[i]-shiftk)<97) {
 					ch4 = (char)(((int)ch2[i]-shiftk-97+26)%26+97);
